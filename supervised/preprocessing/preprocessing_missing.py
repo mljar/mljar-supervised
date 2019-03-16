b98b08e9057490f77c5af8663222d1cec8a9fcc9
Piotr Plonski <pplonski86@gmail.com>
2019-03-16 11:10:59 +0100

    update in preprocessing

--- supervised/preprocessing/preprocessing_missing.py
+++ supervised/preprocessing/preprocessing_missing.py
@@ -16,59 +16,29 @@ class PreprocessingMissingValues(object):
     NA_EXCLUDE = "na_exclude"
     MISSING_VALUE = "_missing_value_"
 
-    def __init__(self, na_fill_method=FILL_NA_MEDIAN):
+
+    def __init__(self, columns = [], na_fill_method = FILL_NA_MEDIAN):
+        self._columns = columns
         # fill method
         self._na_fill_method = na_fill_method
         # fill parameters stored as a dict, feature -> fill value
-        self._na_fill_params = None
+        self._na_fill_params = {}
 
-    # for one column
-    def fit(self, x):
-        x = self._fit_na_fill(x)
 
-    def _fit_na_fill(self, x):
-        if np.sum(pd.isnull(x) == True) == 0:
-            return
-        self._na_fill_params = self._get_fill_value(x)
+    def fit(self, X):
+        X = self._fit_na_fill(X)
 
-    def transform(self, x):
-        x = self._transform_na_fill(x)
-        # this is additional run through columns,
-        # in case of transforming data with new columns with missing values
-        x = self._make_sure_na_filled(x)
-        return x
-
-    def _transform_na_fill(self, x):
-        value = self._na_fill_params
-        ind = pd.isnull(x)
-        print(x.is_copy, "copy")
-        x.loc[ind] = value ###########################x.loc[ind]
-        return x
-
-    def to_json(self):
-        # prepare json with all parameters
-        if self._na_fill_params is None:
-            return {}
-        params = {
-            "fill_method": self._na_fill_method,
-            "fill_params": self._na_fill_params,
-        }
-        return params
-
-    def from_json(self, params):
-        if params is not None:
-            self._na_fill_method = params.get("fill_method", None)
-            self._na_fill_params = params.get("fill_params", None)
-        else:
-            self._na_fill_method, self._na_fill_params = None, None
+    def _fit_na_fill(self, X):
+        for column in self._columns:
+            if np.sum(pd.isnull(X[column]) == True) == 0:
+                continue
+            self._na_fill_params[column] = self._get_fill_value(X[column])
 
     def _get_fill_value(self, x):
         # categorical type
         if PreprocessingUtils.get_type(x) == PreprocessingUtils.CATEGORICAL:
             if self._na_fill_method == PreprocessingMissingValues.FILL_NA_MIN:
-                return (
-                    PreprocessingMissingValues.MISSING_VALUE
-                )  # add new categorical value
+                return PreprocessingMissingValues.MISSING_VALUE # add new categorical value
             return PreprocessingUtils.get_most_frequent(x)
         # numerical type
         if self._na_fill_method == PreprocessingMissingValues.FILL_NA_MIN:
@@ -77,6 +47,36 @@ class PreprocessingMissingValues(object):
             return PreprocessingUtils.get_mean(x)
         return PreprocessingUtils.get_median(x)
 
-    def _make_sure_na_filled(self, x):
-        self._fit_na_fill(x)
-        return self._transform_na_fill(x)
+    def transform(self, X):
+        X = self._transform_na_fill(X)
+        # this is additional run through columns,
+        # in case of transforming data with new columns with missing values
+        # X = self._make_sure_na_filled(X) # disbaled for now
+        return X
+
+    def _transform_na_fill(self, X):
+        for column, value in self._na_fill_params.items():
+            ind = pd.isnull(X.loc[:,column])
+            X.loc[ind, column] = value
+        return X
+
+    def _make_sure_na_filled(self, X):
+        self._fit_na_fill(X)
+        return self._transform_na_fill(X)
+
+    def to_json(self):
+        # prepare json with all parameters
+        if len(self._na_fill_params) == 0:
+            return {}
+        params = {
+                    'fill_method': self._na_fill_method,
+                    'fill_params': self._na_fill_params
+                }
+        return params
+
+    def from_json(self, params):
+        if params is not None:
+            self._na_fill_method = params.get('fill_method', None)
+            self._na_fill_params = params.get('fill_params', {})
+        else:
+            self._na_fill_method, self._na_fill_params = None, None
--- tests/tests_preprocessing/test_preprocessing_missing.py
+++ tests/tests_preprocessing/test_preprocessing_missing.py
@@ -18,7 +18,7 @@ class PreprocessingMissingValuesTest(unittest.TestCase):
             preprocess_missing._na_fill_method,
             PreprocessingMissingValues.FILL_NA_MEDIAN,
         )
-        self.assertTrue(preprocess_missing._na_fill_params is None)
+        self.assertEqual(preprocess_missing._na_fill_params, {})
 
     def test_get_fill_value(self):
         """
@@ -29,18 +29,21 @@ class PreprocessingMissingValuesTest(unittest.TestCase):
         df = pd.DataFrame(data=d)
         # fill with median
         preprocess_missing = PreprocessingMissingValues(
+            df.columns,
             PreprocessingMissingValues.FILL_NA_MEDIAN
         )
         self.assertEqual(preprocess_missing._get_fill_value(df["col1"]), 2)
         self.assertEqual(preprocess_missing._get_fill_value(df["col2"]), "a")
         # fill with mean
         preprocess_missing = PreprocessingMissingValues(
+            df.columns,
             PreprocessingMissingValues.FILL_NA_MEDIAN
         )
         self.assertEqual(preprocess_missing._get_fill_value(df["col1"]), 2)
         self.assertEqual(preprocess_missing._get_fill_value(df["col2"]), "a")
         # fill with min
         preprocess_missing = PreprocessingMissingValues(
+            df.columns,
             PreprocessingMissingValues.FILL_NA_MIN
         )
         self.assertEqual(preprocess_missing._get_fill_value(df["col1"]), 0)
@@ -58,47 +61,43 @@ class PreprocessingMissingValuesTest(unittest.TestCase):
             "col3": ["a", "a", "d", "b", "c"],
         }
         df = pd.DataFrame(data=d)
-
-        for col in df.columns:
-            # fill with median
-            preprocess_missing = PreprocessingMissingValues(
-                PreprocessingMissingValues.FILL_NA_MEDIAN
-            )
-            preprocess_missing._fit_na_fill(df[col])
-            if col == "col1":
-                self.assertEqual(2, preprocess_missing._na_fill_params)
-            if col == "col2":
-                self.assertEqual("a", preprocess_missing._na_fill_params)
-            if col == "col3":
-                self.assertTrue(preprocess_missing._na_fill_params is None)
-
-        for col in df.columns:
-            # fill with mean
-            preprocess_missing = PreprocessingMissingValues(
-                PreprocessingMissingValues.FILL_NA_MEAN
-            )
-            preprocess_missing._fit_na_fill(df[col])
-            if col == "col1":
-                self.assertEqual(2, preprocess_missing._na_fill_params)
-            if col == "col2":
-                self.assertEqual("a", preprocess_missing._na_fill_params)
-            if col == "col3":
-                self.assertTrue(preprocess_missing._na_fill_params is None)
-
-        for col in df.columns:
-            # fill with min
-            preprocess_missing = PreprocessingMissingValues(
-                PreprocessingMissingValues.FILL_NA_MIN
-            )
-            preprocess_missing._fit_na_fill(df[col])
-            if col == "col1":
-                self.assertEqual(0, preprocess_missing._na_fill_params)
-            if col == "col2":
-                self.assertEqual("_missing_value_", preprocess_missing._na_fill_params)
-            if col == "col3":
-                self.assertTrue(preprocess_missing._na_fill_params is None)
+        # fill with median
+        preprocess_missing = PreprocessingMissingValues(
+            df.columns,
+            PreprocessingMissingValues.FILL_NA_MEDIAN
+        )
+        preprocess_missing._fit_na_fill(df)
+        self.assertTrue("col1" in preprocess_missing._na_fill_params)
+        self.assertTrue("col2" in preprocess_missing._na_fill_params)
+        self.assertTrue("col3" not in preprocess_missing._na_fill_params)
+        self.assertEqual(2, preprocess_missing._na_fill_params["col1"])
+        self.assertEqual("a", preprocess_missing._na_fill_params["col2"])
+        # fill with mean
+        preprocess_missing = PreprocessingMissingValues(
+            df.columns,
+            PreprocessingMissingValues.FILL_NA_MEAN
+        )
+        preprocess_missing._fit_na_fill(df)
+        self.assertTrue("col1" in preprocess_missing._na_fill_params)
+        self.assertTrue("col2" in preprocess_missing._na_fill_params)
+        self.assertTrue("col3" not in preprocess_missing._na_fill_params)
+        self.assertEqual(2, preprocess_missing._na_fill_params["col1"])
+        self.assertEqual("a", preprocess_missing._na_fill_params["col2"])
+        # fill with min
+        preprocess_missing = PreprocessingMissingValues(
+            df.columns,PreprocessingMissingValues.FILL_NA_MIN
+        )
+        preprocess_missing._fit_na_fill(df)
+        self.assertTrue("col1" in preprocess_missing._na_fill_params)
+        self.assertTrue("col2" in preprocess_missing._na_fill_params)
+        self.assertTrue("col3" not in preprocess_missing._na_fill_params)
+        self.assertEqual(0, preprocess_missing._na_fill_params["col1"])
+        self.assertEqual("_missing_value_", preprocess_missing._na_fill_params["col2"])
 
     def test_transform(self):
+        """
+            Check transform
+        """
         # training data
         d = {
             "col1": [1, 2, 3, np.nan, np.nan],
@@ -115,29 +114,61 @@ class PreprocessingMissingValuesTest(unittest.TestCase):
             "col4": ["b", "b", np.nan, "b", "c"],
         }
         df_test = pd.DataFrame(data=d_test)
+        # fill with median
+        preprocess_missing = PreprocessingMissingValues(
+            df.columns,PreprocessingMissingValues.FILL_NA_MEDIAN
+        )
+        preprocess_missing.fit(df)
+        self.assertEqual(
+            2, len(preprocess_missing._na_fill_params)
+        )  # there should be only two columns
+        df_transformed = preprocess_missing.transform(df_test)
+        self.assertTrue(
+            np.isnan(df.loc[3, "col1"])
+        )  # training data frame is not filled
+        self.assertEqual(
+            2, df_test.loc[3, "col1"]
+        )  # data frame is filled after transform
+        self.assertEqual("a", df_test.loc[2, "col2"])
 
-        for col in df.columns:
-            # fill with median
-            preprocess_missing = PreprocessingMissingValues(
-                PreprocessingMissingValues.FILL_NA_MEDIAN
-            )
-            preprocess_missing.fit(df[col])
-            df_transformed = preprocess_missing.transform(df_test.loc[:,col])
-            if col == "col1":
-                self.assertTrue(
-                    np.isnan(df.loc[3, "col1"])
-                )  # training data frame is not filled
-                self.assertEqual(
-                    2, df_test.loc[3, "col1"]
-                )  # data frame is filled after transform
-            if col == "col2":
-                self.assertEqual("a", df_test.loc[2, "col2"])
-            # columns without missing values in training set are also filled
-            # but they are filled based on their own values
-            if col == "col3":
-                self.assertEqual(2, df_test.loc[3, "col3"])
-            if col == "col4":
-                self.assertEqual("b", df_test.loc[3, "col4"])
+        # it is disabled, should be treated separately at the end of preprocessing
+        # columns without missing values in training set are also filled
+        # but they are filled based on their own values
+        #self.assertEqual(2, df_test.loc[3, "col3"])
+        #self.assertEqual("b", df_test.loc[3, "col4"])
+
+    def test_transform_on_new_data(self):
+        # training data
+        d = {
+            "col1": [1, 1, np.nan, 3],
+            "col2": ["a", "a", np.nan, "a"],
+            "col3": [1, 1, 1, 3],
+            "col4": ["a", "a", "b", "c"],
+            "y": [0, 1, 1, 1],
+        }
+        df = pd.DataFrame(data=d)
+        X_train = df.loc[:, ["col1", "col2", "col3", "col4"]]
+        y_train = df.loc[:, "y"]
+
+        d_test = {
+            "col1": [1, 1, np.nan, 3],
+            "col2": ["a", "a", np.nan, "a"],
+            "col3": [1, 1, 1, 3],
+            "col4": ["a", "a", "b", "c"],
+            "y": [np.nan, 1, np.nan, 1],
+        }
+        df_test = pd.DataFrame(data=d_test)
+        X_test = df_test.loc[:, ["col1", "col2", "col3", "col4"]]
+        y_test = df_test.loc[:, "y"]
+
+        pm = PreprocessingMissingValues(X_train.columns,PreprocessingMissingValues.FILL_NA_MEDIAN)
+        pm.fit(X_train)
+        X_train = pm.transform(X_train)
+        X_test = pm.transform(X_test)
+
+        self.assertEqual(1, X_test.loc[2, "col1"])
+        self.assertEqual("a", X_test.loc[2, "col2"])
+
 
 if __name__ == "__main__":
     unittest.main()
