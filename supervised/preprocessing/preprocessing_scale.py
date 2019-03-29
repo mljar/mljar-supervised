249cf61190a8da6486b03cab8e8c6413abaae9bd
Piotr Plonski <pplonski86@gmail.com>
2019-03-29 10:30:43 +0100

    scale tests

--- supervised/preprocessing/preprocessing_scale.py
+++ supervised/preprocessing/preprocessing_scale.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
-from supervised.preprocessing.preprocessing_utils import PreprocessingUtils
+from sklearn import preprocessing
 
 
 class PreprocessingScale(object):
@@ -12,17 +11,29 @@ class PreprocessingScale(object):
     SCALE_LOG = "scale_log"
     SCALE_LOG_AND_NORMAL = "scale_log_and_normal"
 
-    def __init__(self):
-        pass
+    def __init__(self, columns = []):
+        self.scale = preprocessing.StandardScaler(copy=True, with_mean=True, with_std=True)
+        self.columns = columns
 
     def fit(self, X):
-        pass
+        self.scale.fit(X[self.columns])
 
     def transform(self, X):
-        pass
+        X.loc[:, self.columns] = self.scale.transform(X[self.columns])
+        return X
 
     def to_json(self):
-        pass
+        data_json = {"scale":self.scale.scale_,
+            "mean":self.scale.mean_,
+            "var": self.scale.var_,
+            "n_samples_seen":self.scale.n_samples_seen_,
+            "columns": self.columns}
+        return data_json
 
-    def from_json(self, json_data):
-        pass
+    def from_json(self, data_json):
+        self.scale = preprocessing.StandardScaler(copy=True, with_mean=True, with_std=True)
+        self.scale.scale_ = data_json.get("scale")
+        self.scale.mean_ = data_json.get("mean")
+        self.scale.var_ = data_json.get("var")
+        self.scale.n_samples_seen_ = data_json.get("n_samples_seen")
+        self.columns = data_json.get("columns")
