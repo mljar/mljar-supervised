01d35937752497c07f45373dbf724ac65271b362
Piotrek <pplonski86@gmail.com>
2019-04-23 17:24:23 +0200

    version 0.1.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mljar-supervised',
-    version='0.1.3',
+    version='0.1.4',
     description='Automated Machine Learning for Supervised tasks',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=["numpy>=1.15.4", "pandas>=0.23.4", "scipy>=1.1.0", "scikit-learn>=0.20.0",
                         "xgboost==0.80", "lightgbm==2.2.3", "catboost==0.13.1",
-                        "h5py>=2.9.0", "tensorflow==1.13.1", "Keras==2.2.4"],
+                        "h5py>=2.9.0", "tensorflow==1.13.1", "Keras==2.2.4", "tqdm==4.31.1"],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6'
