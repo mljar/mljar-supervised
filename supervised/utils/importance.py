190a9f6f879b09d78c9f52b5aa4dbd39c10d2673
Piotrek <pplonski86@gmail.com>
2020-04-27 16:24:34 +0200

    add feature importance (#27)

---
+++ supervised/utils/importance.py
@@ -0,0 +1,72 @@
+import os
+import logging
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.inspection import permutation_importance
+from supervised.algorithms.registry import (
+    BINARY_CLASSIFICATION,
+    MULTICLASS_CLASSIFICATION,
+    REGRESSION,
+)
+
+logger = logging.getLogger(__name__)
+from supervised.utils.config import LOG_LEVEL
+
+logger.setLevel(LOG_LEVEL)
+
+
+class PermutationImportance:
+    @staticmethod
+    def compute_and_plot(
+        model,
+        X_validation,
+        y_validation,
+        model_file_path,
+        learner_name,
+        metric_name=None,
+        ml_task=None,
+    ):
+
+        # for scoring check https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
+        if ml_task == BINARY_CLASSIFICATION:
+            scoring = "neg_log_loss"
+        elif ml_task == MULTICLASS_CLASSIFICATION:
+            scoring = "neg_log_loss"
+        else:
+            scoring = "neg_mean_squared_error"
+
+        importance = permutation_importance(
+            model,
+            X_validation,
+            y_validation,
+            scoring=scoring,
+            n_jobs=-1,
+            random_state=12,
+        )
+
+        sorted_idx = importance["importances_mean"].argsort()
+
+        # save detailed importance
+        df_imp = pd.DataFrame(
+            {
+                "feature": X_validation.columns[sorted_idx],
+                "mean_importance": importance["importances_mean"][sorted_idx],
+            }
+        )
+        df_imp.to_csv(
+            os.path.join(model_file_path, f"{learner_name}_importance.csv"), index=False
+        )
+
+        # limit number of column in the plot
+        if len(sorted_idx) > 50:
+            sorted_idx = sorted_idx[-50:]
+        plt.figure(figsize=(10, 7))
+        plt.barh(
+            X_validation.columns[sorted_idx], importance["importances_mean"][sorted_idx]
+        )
+        plt.xlabel("Mean of feature importance")
+        plt.tight_layout(pad=2.0)
+        plot_path = os.path.join(model_file_path, f"{learner_name}_importance.png")
+        plt.savefig(plot_path)
+        plt.close("all")
---
+++ tests/tests_utils/test_importance.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+import numpy as np
+import pandas as pd
+import tempfile
+
+from xgboost import XGBClassifier
+from supervised.utils.importance import PermutationImportance
+
+
+class PermutationImportanceTest(unittest.TestCase):
+    def test_compute_and_plot(self):
+        rows = 20
+        X = np.random.rand(rows, 3)
+        X = pd.DataFrame(X, columns=[f"f{i}" for i in range(3)])
+        y = np.random.randint(0, 2, rows)
+
+        model =XGBClassifier(n_estimators=1, max_depth=2)
+        model.fit(X, y)
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            PermutationImportance.compute_and_plot(
+                model,
+                X_validation = X,
+                y_validation = y,
+                model_file_path = tmpdir,
+                learner_name = "learner_test",
+                metric_name=None,
+                ml_task="binary_classification",
+            )
+            self.assertTrue(os.path.exists(os.path.join(tmpdir,"learner_test_importance.csv")))
+            self.assertTrue(os.path.exists(os.path.join(tmpdir,"learner_test_importance.png")))
+
+        
