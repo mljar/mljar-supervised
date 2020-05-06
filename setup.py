942b8bfc7be0c4d6f66f116ac7594fbd98f50439
Piotrek <pplonski86@gmail.com>
2020-05-06 15:45:17 +0200

    fix time constaint

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mljar-supervised',
-    version='0.3.3',
+    version='0.3.4',
     description='Automated Machine Learning for Supervised tasks',
     long_description=long_description,
     long_description_content_type="text/markdown",
