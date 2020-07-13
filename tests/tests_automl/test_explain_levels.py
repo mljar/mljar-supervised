ce7af22134ae4f4667476995caf19586adfc16f3
Piotrek <pplonski86@gmail.com>
2020-07-13 08:47:34 +0200

    add test for decision tree visualization (#121)

--- tests/tests_automl/test_explain_levels.py
+++ tests/tests_automl/test_explain_levels.py
@@ -216,3 +216,72 @@ class AutoMLExplainLevelsTest(unittest.TestCase):
                 produced = True
                 break
         self.assertFalse(produced)
+
+    def test_build_decision_tree(self):
+        a = AutoML(
+            results_path=self.automl_dir,
+            total_time_limit=1,
+            algorithms=["Decision Tree"],
+            train_ensemble=False,
+            validation={
+                "validation_type": "kfold",
+                "k_folds": 2,
+                "shuffle": True,
+                "stratify": True,
+            },
+            explain_level=2,
+        )
+        a.set_advanced(start_random_models=1)
+
+        X, y = datasets.make_regression(
+            n_samples=100, n_features=5, n_informative=4, shuffle=False, random_state=0
+        )
+        X = pd.DataFrame(X, columns=[f"f_{i}" for i in range(X.shape[1])])
+
+        a.fit(X, y)
+
+        result_files = os.listdir(os.path.join(self.automl_dir, "1_DecisionTree"))
+        
+        # There should be files with:
+        # - decision tree visualization
+        # - permutation importance
+        # - shap importance
+        # - shap dependence
+        # - shap decisions
+
+        # Check Decision Tree visualization
+        produced = False
+        for f in result_files:
+            if "tree.svg" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check permutation importance
+        produced = False
+        for f in result_files:
+            if "importance.csv" in f and "shap" not in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check shap importance
+        produced = False
+        for f in result_files:
+            if "importance.csv" in f and "shap" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check shap dependence
+        produced = False
+        for f in result_files:
+            if "dependence.png" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check shap decisions
+        produced = False
+        for f in result_files:
+            if "decisions.png" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        
