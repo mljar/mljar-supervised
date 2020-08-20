1df5fabd94ba64a2aa249f84672617068a263384
Shahul E S <shahules786@gmail.com>
2020-08-20 20:12:24 +0530

    make eda static

--- supervised/preprocessing/eda.py
+++ supervised/preprocessing/eda.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 from wordcloud import WordCloud
 from wordcloud import STOPWORDS
+from collections import defaultdict
 
 from supervised.algorithms.registry import BINARY_CLASSIFICATION
 from supervised.algorithms.registry import MULTICLASS_CLASSIFICATION
@@ -17,53 +18,50 @@ from supervised.preprocessing.preprocessing_utils import PreprocessingUtils
 class EDA:
 
 
-    def __init__(self,X,y,ml_task, eda_path ):
 
-        self.X_train = X
-        self.y_train = y
-        self._ml_task = ml_task
-        self.eda_path = eda_path
+    @staticmethod
+    def compute(X_train,y_train,eda_path):
 
+        inform = defaultdict(list)
 
-    
-    def compute(self,X_train,y_train):
+        if isinstance(y_train,pd.Series):
 
-        inform = {}
+            if PreprocessingUtils.get_type(y_train) in ('categorical') :
 
-        if self._ml_task in [BINARY_CLASSIFICATION, MULTICLASS_CLASSIFICATION]:
+                plt.figure(figsize=(5, 5))
+                sns.countplot(y_train, color='blue')
+                plt.title("Target class distribution")
+                plt.tight_layout(pad=2.0)
+                plot_path = os.path.join(eda_path, "target.png")
+                plt.savefig(plot_path)
+                plt.close("all")
 
-            plt.figure(figsize=(5, 5))
-            sns.countplot(y_train, color='blue')
-            plt.title("Target class distribution")
-            plt.tight_layout(pad=2.0)
-            plot_path = os.path.join(self.eda_path, "target.png")
-            plt.savefig(plot_path)
-            plt.close("all")
+            
+            else :
 
-          
-        elif self._ml_task == REGRESSION:
-
-            plt.figure(figsize=(5, 5))
-            sns.distplot(y_train, color='blue')
-            plt.title("Target class distribution")
-            plt.tight_layout(pad=2.0)
-            plot_path = os.path.join(self.eda_path, "target.png")
-            plt.savefig(plot_path)
-            plt.close("all")
+                plt.figure(figsize=(5, 5))
+                sns.distplot(y_train, color='blue')
+                plt.title("Target class distribution")
+                plt.tight_layout(pad=2.0)
+                plot_path = os.path.join(eda_path, "target.png")
+                plt.savefig(plot_path)
+                plt.close("all")
 
 
-        inform["missing"] = [pd.isnull(y_train).sum() / y_train.shape[0]]
-        inform["unique"] = [y_train.nunique()]
-        inform['feature_type'] = [PreprocessingUtils.get_type(y_train)]
-        inform['plot'] = ['![](target.png)']
-        inform['feature'] = ["target"]
-        inform['desc'] = [y_train.describe().to_dict()]
+            inform["missing"].append(pd.isnull(y_train).sum() / y_train.shape[0])
+            inform["unique"].append(y_train.nunique())
+            inform['feature_type'].append(PreprocessingUtils.get_type(y_train))
+            inform['plot'].append('![](target.png)')
+            inform['feature'].append("target")
+            inform['desc'].append(y_train.describe().to_dict())
+
+
 
         for col in X_train.columns:
 
-            inform['feature_type'] += [
+            inform['feature_type'].append( 
                 PreprocessingUtils.get_type(
-                    X_train[col])]
+                    X_train[col]))
 
             if PreprocessingUtils.get_type(
                     X_train[col]) in (
@@ -75,7 +73,7 @@ class EDA:
                     X_train[col], order=X_train[col].value_counts().iloc[:10].index, color='blue')
                 plt.title(f"{col} class distribution")
                 plt.tight_layout(pad=2.0)
-                plot_path = os.path.join(self.eda_path, f"{col}.png")
+                plot_path = os.path.join(eda_path, f"{col}.png")
                 plt.savefig(plot_path)
                 plt.close("all")
 
@@ -86,7 +84,7 @@ class EDA:
                 sns.distplot(X_train[col], color='blue')
                 plt.title(f"{col} value distribution")
                 plt.tight_layout(pad=2.0)
-                plot_path = os.path.join(self.eda_path, f"{col}.png")
+                plot_path = os.path.join(eda_path, f"{col}.png")
                 plt.savefig(plot_path)
                 plt.close("all")
 
@@ -106,31 +104,31 @@ class EDA:
 
                 plt.imshow(wordcloud, aspect="auto", interpolation="nearest")
                 plt.axis('off')
-                plot_path = os.path.join(self.eda_path, f"{col}.png")
+                plot_path = os.path.join(eda_path, f"{col}.png")
                 plt.savefig(plot_path)
 
             elif PreprocessingUtils.get_type(X_train[col]) in ('datetime'):
 
                 plt.figure(figsize=(5, 5))
-                pd.to_datetime(df[col]).plot(grid='True', color='blue')
+                pd.to_datetime(X_train[col]).plot(grid='True', color='blue')
                 plt.tight_layout(pad=2.0)
-                plot_path = os.path.join(self.eda_path, f"{col}.png")
+                plot_path = os.path.join(eda_path, f"{col}.png")
                 plt.savefig(plot_path)
                 plt.close("all")
 
 
 
-            inform["missing"] += [pd.isnull(X_train[col]).sum()
-                                      * 100 / X_train.shape[0]]
+            inform["missing"].append(pd.isnull(X_train[col]).sum()
+                                      * 100 / X_train.shape[0])
 
-            inform["unique"] += [int(X_train[col].nunique())]
-            inform['plot'] += [f'![]({col}.png)']
-            inform['feature'] += [str(col)]
-            inform['desc'] += [X_train[col].describe().to_dict()]
+            inform["unique"].append(int(X_train[col].nunique()))
+            inform['plot'].append(f'![]({col}.png)')
+            inform['feature'].append(str(col))
+            inform['desc'].append(X_train[col].describe().to_dict())
 
         df = pd.DataFrame(inform)
 
-        with open(os.path.join(self.eda_path, "Readme.md"), "w") as fout:
+        with open(os.path.join(eda_path, "Readme.md"), "w") as fout:
 
             for i, row in df.iterrows():
 
