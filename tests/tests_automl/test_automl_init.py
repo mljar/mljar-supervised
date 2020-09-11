61090eae37ab5366d9785c0a83f262e48ff511bc
Piotrek <pplonski86@gmail.com>
2020-09-11 17:13:59 +0200

    :boom: remove the tuning_mode parameter from AutoML constructor (#175)

---
+++ tests/tests_automl/test_automl_init.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+import tempfile
+import json
+import numpy as np
+import pandas as pd
+import shutil
+
+from supervised import AutoML
+from supervised.exceptions import AutoMLException
+
+class AutoMLInitTest(unittest.TestCase):
+
+    automl_dir = "automl_testing"
+
+    def tearDown(self):
+        shutil.rmtree(self.automl_dir, ignore_errors=True)
+
+
+    def test_custom_init(self):
+
+        X = np.random.uniform(size=(30, 2))
+        y = np.random.randint(0, 2, size=(30,))
+
+        
+        automl = AutoML(
+            results_path=self.automl_dir,
+            model_time_limit=1,
+            algorithms=["Xgboost"],
+            explain_level=0,
+            train_ensemble=False,
+            stack_models=False,
+            validation_strategy={
+                "validation_type": "split"
+            },
+            start_random_models=3,
+            hill_climbing_steps=1,
+            top_models_to_improve=1
+        )
+
+        automl.fit(X, y)
+        self.assertGreater(len(automl._models), 4)
