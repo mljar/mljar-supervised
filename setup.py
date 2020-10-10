964e54690c0502150b78269d9e8e6aaf5e5de221
Piotr <pplonski86@gmail.com>
2020-10-10 12:58:54 +0200

    :arrow_up: add python 3.8 and 3.9 (#220)

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords=[
         "automl",
