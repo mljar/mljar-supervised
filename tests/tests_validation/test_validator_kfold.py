6ed1d163157f1b326dbc4f8887be69314ceb5b8e
Piotrek <pplonski86@gmail.com>
2020-12-08 09:24:00 +0100

    add tests for repeated validation (#223)

--- tests/tests_validation/test_validator_kfold.py
+++ tests/tests_validation/test_validator_kfold.py
@@ -8,18 +8,16 @@ import shutil
 
 
 class KFoldValidatorTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        print("SETUP CLASS")
-        cls._results_path = "/tmp/k_fold_test"
-        os.mkdir(cls._results_path)
+    
+    def setUp(self):
+        self._results_path = "/tmp/k_fold_test"
+        os.mkdir(self._results_path)
 
-    @classmethod
-    def tearDownClass(cls):
-        print("TEAR DOWN CLASS")
-
-        shutil.rmtree(cls._results_path, ignore_errors=True)
-
+    
+    def tearDown(self):
+        shutil.rmtree(self._results_path, ignore_errors=True)
+    
+    
     def test_create(self):
 
         data = {
@@ -37,7 +35,7 @@ class KFoldValidatorTest(unittest.TestCase):
 
         params = {
             "shuffle": False,
-            "stratify": False,
+            "stratify": True,
             "k_folds": 2,
             "results_path": self._results_path,
             "X_path": X_path,
@@ -77,7 +75,7 @@ class KFoldValidatorTest(unittest.TestCase):
         data["y"].to_parquet(y_path, index=False)
 
         params = {
-            "shuffle": True,
+            "shuffle": False,
             "stratify": True,
             "k_folds": 2,
             "results_path": self._results_path,
@@ -93,10 +91,10 @@ class KFoldValidatorTest(unittest.TestCase):
             X_train, y_train = train.get("X"), train.get("y")
             X_validation, y_validation = validation.get("X"), validation.get("y")
 
-            self.assertEqual(X_train.shape[0], 2)
-            self.assertEqual(y_train.shape[0], 2)
-            self.assertEqual(X_validation.shape[0], 2)
-            self.assertEqual(y_validation.shape[0], 2)
+            self.assertEqual(X_train.shape[0], 3)
+            self.assertEqual(y_train.shape[0], 3)
+            self.assertEqual(X_validation.shape[0], 3)
+            self.assertEqual(y_validation.shape[0], 3)
 
     def test_create_with_target_as_labels(self):
 
@@ -134,3 +132,47 @@ class KFoldValidatorTest(unittest.TestCase):
             self.assertEqual(y_train.shape[0], 2)
             self.assertEqual(X_validation.shape[0], 2)
             self.assertEqual(y_validation.shape[0], 2)
+
+
+
+    def test_repeats(self):
+
+        data = {
+            "X": pd.DataFrame(
+                np.array([[0, 0], [0, 1], [1, 0], [1, 1]]), columns=["a", "b"]
+            ),
+            "y": pd.DataFrame(np.array([0, 0, 1, 1]), columns=["target"]),
+        }
+
+        X_path = os.path.join(self._results_path, "X.parquet")
+        y_path = os.path.join(self._results_path, "y.parquet")
+
+        data["X"].to_parquet(X_path, index=False)
+        data["y"].to_parquet(y_path, index=False)
+
+        params = {
+            "shuffle": False,
+            "stratify": False,
+            "k_folds": 2,
+            "repeat": 3,
+            "results_path": self._results_path,
+            "X_path": X_path,
+            "y_path": y_path,
+        }
+        vl = KFoldValidator(params)
+
+        self.assertEqual(params["k_folds"], vl.get_n_splits())
+        self.assertEqual(params["repeat"], vl.get_repeats())
+        
+        # for train, validation in vl.split():
+        for repeat in range(vl.get_repeats()):
+            for k_fold in range(vl.get_n_splits()):
+                train, validation = vl.get_split(k_fold, repeat)
+
+                X_train, y_train = train.get("X"), train.get("y")
+                X_validation, y_validation = validation.get("X"), validation.get("y")
+
+                self.assertEqual(X_train.shape[0], 2)
+                self.assertEqual(y_train.shape[0], 2)
+                self.assertEqual(X_validation.shape[0], 2)
+                self.assertEqual(y_validation.shape[0], 2)
