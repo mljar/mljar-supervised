ac21fe822ed10dc565f829eb0b7b388861b812dc
Piotrek <pplonski86@gmail.com>
2020-12-08 12:04:06 +0100

    reduce number of folds for small datasets in compete mode (#266)

--- supervised/base_automl.py
+++ supervised/base_automl.py
@@ -668,6 +668,9 @@ class BaseAutoML(BaseEstimator, ABC):
             k_folds = 5
             if folds_cnt >= 15:
                 k_folds = 10
+            # too small dataset to run 10-fold CV
+            if k_folds == 10 and self.n_rows_in_ <= 1000:
+                k_folds = 5
             self._validation_strategy["validation_type"] = "kfold"
             del self._validation_strategy["train_ratio"]
             self._validation_strategy["k_folds"] = k_folds
