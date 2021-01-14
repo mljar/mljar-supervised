58576f432f377ef3e786b23d706b4fd1e3f4afdd
Piotrek <pplonski86@gmail.com>
2021-01-14 22:01:24 +0100

    report in notebook (#298)

--- supervised/base_automl.py
+++ supervised/base_automl.py
@@ -1637,36 +1637,32 @@ margin-right: auto;display: block;"/>\n\n"""
                     """
 .styled-table {
     border-collapse: collapse;
-    margin: 25px 0;
     font-size: 0.9em;
-    font-family: sans-serif;
-    min-width: 400px;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+    font-family:Courier New;
 }
-.styled-table thead tr {
-    background-color: #0099cc;
-    color: #ffffff;
+
+.styled-table td, .styled-table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+.styled-table tr:nth-child(even){background-color: #f2f2f2;}
+
+.styled-table tr:hover {background-color: #e0ecf5;}
+
+.styled-table th {
+    padding-top: 6px;
+    padding-bottom: 6px;
     text-align: left;
-}
-.styled-table th,
-.styled-table td {
-    padding: 8px 10px;
-}
-.styled-table tbody tr {
-    border-bottom: 1px solid #dddddd;
+    background-color: #0099cc;
+    color: white;
 }
 
-.styled-table tbody tr:nth-of-type(even) {
-    background-color: #e0ecf5;
+body {
+    font-family: Arial;
+    background-color: rgba(236, 243, 249, 0.15);
 }
 
-.styled-table tbody tr:last-of-type {
-    border-bottom: 2px solid #0099cc;
-}
-.styled-table tbody tr.active-row {
-    font-weight: bold;
-    color: #0099cc;
-}
 """
                 )
 
