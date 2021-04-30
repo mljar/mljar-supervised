3ab1c4495bd0e482dc03350b6edd4ace25469890
Piotr <pplonski86@gmail.com>
2021-04-30 15:12:17 +0200

    add models correlation (#386)

--- supervised/utils/automl_plots.py
+++ supervised/utils/automl_plots.py
@@ -2,7 +2,7 @@ import os
 import logging
 import numpy as np
 import pandas as pd
-
+import scipy as sp
 logger = logging.getLogger(__name__)
 from supervised.utils.metric import Metric
 from supervised.utils.config import LOG_LEVEL
@@ -18,6 +18,7 @@ warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 class AutoMLPlots:
 
     features_heatmap_fname = "features_heatmap.png"
+    correlation_heatmap_fname = "correlation_heatmap.png"
 
     @staticmethod
     def add(results_path, models, fout):
@@ -32,6 +33,18 @@ class AutoMLPlots:
             fout.write(
                 f"![features importance across models]({AutoMLPlots.features_heatmap_fname})\n\n"
             )
+
+        AutoMLPlots.models_correlation(results_path, models)
+
+        correlation_plot_path = os.path.join(
+            results_path, AutoMLPlots.correlation_heatmap_fname
+        )
+        if os.path.exists(correlation_plot_path):
+            fout.write("\n\n### Spearman Correlation of Models\n")
+            fout.write(
+                f"![models spearman correlation]({AutoMLPlots.correlation_heatmap_fname})\n\n"
+            )
+
 
     @staticmethod
     def models_feature_importance(results_path, models):
@@ -90,7 +103,61 @@ class AutoMLPlots:
             ax.set_title(title)
 
             plt.tight_layout(pad=2.0)
-            plot_path = os.path.join(results_path, "features_heatmap.png")
+            plot_path = os.path.join(results_path, AutoMLPlots.features_heatmap_fname)
+            plt.savefig(plot_path)
+            plt.close("all")
+        except Exception as e:
+            pass
+
+
+    @staticmethod
+    def correlation(oof1, oof2):
+        cols = [c for c in oof1.columns if "prediction" in c]
+
+        v = []
+        for c in cols:
+            c, _ = sp.stats.spearmanr(oof1[c], oof2[c])
+            v += [c]
+
+        return np.mean(v)
+
+    @staticmethod
+    def models_correlation(results_path, models):
+        try:
+            if len(models) < 2:
+                return
+
+            names = []
+            oofs = []
+            for m in models:
+                names += [m.get_name()]
+                oofs += [m.get_out_of_folds()]
+
+            corrs = np.ones((len(names), len(names)))
+            for i in range(len(names)):
+                for j in range(i+1, len(names)):
+                    corrs[i,j] = corrs[j,i] = AutoMLPlots.correlation(oofs[i], oofs[j])
+            
+            fig, ax = plt.subplots(1, 1, figsize=(10, 9))
+
+            image = ax.imshow(
+                corrs,
+                interpolation="nearest",
+                cmap=plt.cm.get_cmap("Blues"),
+                aspect="auto",
+            )
+            plt.colorbar(mappable=image)
+
+            x_tick_marks = np.arange(len(names))
+            y_tick_marks = np.arange(len(names))
+            ax.set_xticks(x_tick_marks)
+            ax.set_xticklabels(names, rotation=90)
+            ax.set_yticks(y_tick_marks)
+            ax.set_yticklabels(names)
+            ax.set_title("Spearman Correlation of Models")
+
+            plt.tight_layout(pad=2.0)
+            plot_path = os.path.join(results_path, AutoMLPlots.correlation_heatmap_fname)
             plt.savefig(plot_path)
             plt.close("all")
         except Exception as e:
