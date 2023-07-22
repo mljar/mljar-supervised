43aacb1d53e3a77b83d7e371df32845ab91aee4c
Hk669 <hrushi669@gmail.com>
2023-07-22 16:45:34 +0530

    Added dtreeviz to visualize the DecisionTree

--- tests/tests_automl/test_explain_levels.py
+++ tests/tests_automl/test_explain_levels.py
@@ -6,8 +6,11 @@ import numpy as np
 import pandas as pd
 import shutil
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 from supervised import AutoML
+
+from dtreeviz.trees import dtreeviz
 
 from supervised.algorithms.random_forest import additional
 
@@ -254,73 +257,88 @@ class AutoMLExplainLevelsTest(unittest.TestCase):
                 break
         self.assertFalse(produced)
 
-    # def test_build_decision_tree(self):
-    #     a = AutoML(
-    #         results_path=self.automl_dir,
-    #         total_time_limit=10,
-    #         algorithms=["Decision Tree"],
-    #         train_ensemble=False,
-    #         validation_strategy={
-    #             "validation_type": "kfold",
-    #             "k_folds": 2,
-    #             "shuffle": True,
-    #             "stratify": True,
-    #         },
-    #         explain_level=2,
-    #         start_random_models=1,
-    #     )
+    def test_build_decision_tree(self):
+        a = AutoML(
+             results_path=self.automl_dir,
+             total_time_limit=10,
+             algorithms=["Decision Tree"],
+             train_ensemble=False,
+             validation_strategy={
+                 "validation_type": "kfold",
+                 "k_folds": 2,
+                 "shuffle": True,
+                "stratify": True,
+            },
+            explain_level=2,
+            start_random_models=1,
+        )
 
-    #     X, y = datasets.make_regression(
-    #         n_samples=100, n_features=5, n_informative=4, shuffle=False, random_state=0
-    #     )
-    #     X = pd.DataFrame(X, columns=[f"f_{i}" for i in range(X.shape[1])])
+        X, y = datasets.make_regression(
+            n_samples=100, n_features=5, n_informative=4, shuffle=False, random_state=0)
+        X = pd.DataFrame(X, columns=[f"f_{i}" for i in range(X.shape[1])])
 
-    #     a.fit(X, y)
+        X_train,X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+        a.fit(X_train, y_train)
 
-    #     result_files = os.listdir(os.path.join(self.automl_dir, "1_DecisionTree"))
+        result_files = os.listdir(os.path.join(self.automl_dir, "1_DecisionTree"))
 
-    #     # There should be files with:
-    #     # - decision tree visualization
-    #     # - permutation importance
-    #     # - shap importance
-    #     # - shap dependence
-    #     # - shap decisions
+        # There should be files with:
+        # - decision tree visualization
+        # - permutation importance
+        # - shap importance
+        # - shap dependence
+        # - shap decisions
 
-    #     # Check Decision Tree visualization
-    #     produced = False
-    #     for f in result_files:
-    #         if "tree.svg" in f:
-    #             produced = True
-    #             break
-    #     # disable 
-    #     #self.assertTrue(produced)
+        # Check Decision Tree visualization
+        produced = False
+        for f in result_files:
+            if "tree.svg" in f:
+                produced = True
+                break
+        # disable 
+        #self.assertTrue(produced)
 
 
-    #     # Check permutation importance
-    #     produced = False
-    #     for f in result_files:
-    #         if "importance.csv" in f and "shap" not in f:
-    #             produced = True
-    #             break
-    #     self.assertTrue(produced)
-    #     # Check shap importance
-    #     produced = False
-    #     for f in result_files:
-    #         if "importance.csv" in f and "shap" in f:
-    #             produced = True
-    #             break
-    #     self.assertTrue(produced)
-    #     # Check shap dependence
-    #     produced = False
-    #     for f in result_files:
-    #         if "dependence.png" in f:
-    #             produced = True
-    #             break
-    #     self.assertTrue(produced)
-    #     # Check shap decisions
-    #     produced = False
-    #     for f in result_files:
-    #         if "decisions.png" in f:
-    #             produced = True
-    #             break
-    #     self.assertTrue(produced)
+        # Check permutation importance
+        produced = False
+        for f in result_files:
+            if "importance.csv" in f and "shap" not in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check shap importance
+        produced = False
+        for f in result_files:
+            if "importance.csv" in f and "shap" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check shap dependence
+        produced = False
+        for f in result_files:
+            if "dependence.png" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+        # Check shap decisions
+        produced = False
+        for f in result_files:
+            if "decisions.png" in f:
+                produced = True
+                break
+        self.assertTrue(produced)
+
+        # dtreeviz visualization of DecisionTree
+        clf = a.get_best_model()
+        viz = dtreeviz(
+            clf,
+            X_train,
+            y_train,
+            feature_names=X_train.columns,
+            target_name = 'target',
+            class_names = ["target"],
+            fancy=True
+        )
+
+        # to save the visualization
+        viz.save(os.path.join(self.automl_dir,"1_DecisionTree","tree.svg"))
