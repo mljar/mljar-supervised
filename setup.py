6b8b2127a509b32bc0c00b3875d7bafa569c643f
Piotr <pplonski86@gmail.com>
2023-09-21 15:58:53 +0200

    set version 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="mljar-supervised",
-    version="1.0.2",
+    version="1.1.0",
     description="Automated Machine Learning for Humans",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- supervised/__init__.py
+++ supervised/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 from supervised.automl import AutoML
