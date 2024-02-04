d44f868614bc885c3082dd3928b4211aa8d3952a
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-02-04 14:34:14 +0200

    callback code reformat plus type annotations.

--- supervised/callbacks/callback_list.py
+++ supervised/callbacks/callback_list.py
@@ -1,32 +1,39 @@
-class CallbackList(object):
-    def __init__(self, callbacks=[]):
-        self.callbacks = callbacks
+from typing import List
 
-    def add_and_set_learner(self, learner):
+from supervised.algorithms.algorithm import BaseAlgorithm
+from supervised.callbacks.callback import Callback
+
+
+class CallbackList(object):
+
+    def __init__(self, callbacks: List[Callback] = []):
+        self.callbacks: List[Callback] = callbacks
+
+    def add_and_set_learner(self, learner: BaseAlgorithm) -> None:
         for cb in self.callbacks:
             cb.add_and_set_learner(learner)
 
-    def on_learner_train_start(self, logs=None):
+    def on_learner_train_start(self, logs: dict = None) -> None:
         for cb in self.callbacks:
             cb.on_learner_train_start(logs)
 
-    def on_learner_train_end(self, logs=None):
+    def on_learner_train_end(self, logs: dict = None) -> None:
         for cb in self.callbacks:
             cb.on_learner_train_end(logs)
 
-    def on_iteration_start(self, logs=None):
+    def on_iteration_start(self, logs: dict = None) -> None:
         for cb in self.callbacks:
             cb.on_iteration_start(logs)
 
-    def on_iteration_end(self, logs=None, predictions=None):
+    def on_iteration_end(self, logs: dict = None, predictions: dict = None) -> None:
         for cb in self.callbacks:
             cb.on_iteration_end(logs, predictions)
 
-    def on_framework_train_end(self, logs=None):
+    def on_framework_train_end(self, logs: dict = None) -> None:
         for cb in self.callbacks:
             cb.on_framework_train_end(logs)
 
-    def get(self, callback_name):
+    def get(self, callback_name: str) -> Callback:
         for cb in self.callbacks:
             if cb.name == callback_name:
                 return cb
