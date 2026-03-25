1342ed3acdc1edbabebc0397d7d8b9494698121e
Piotr <pplonski86@gmail.com>
2026-03-25 13:24:36 +0100

    fix: add numpy 2.4 in1d compatibility shim

--- supervised/__init__.py
+++ supervised/__init__.py
@@ -1,3 +1,14 @@
 __version__ = "1.2.0"
 
+import numpy as np
+
+
+def _ensure_numpy_compat():
+    if not hasattr(np, "in1d"):
+        # NumPy 2.4 removed np.in1d; some dependencies still use it.
+        np.in1d = np.isin
+
+
+_ensure_numpy_compat()
+
 from supervised.automl import AutoML
